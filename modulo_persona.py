db825fe86ec90f8f48208c1141b4c96e110c048e
Agustin Tocarczuk <agustintocarczuk@gmail.com>
2022-11-06 13:54:47 -0300

    Modulo persona creado

---
+++ modulo_persona.py
@@ -0,0 +1,11 @@
+class Persnona:
+
+        def __init__(self, nombre, apellido):
+
+            self.nombre = nombre
+            self.apellido = apellido
+
+        def __str__(self):
+
+            return self.nombre + " " + self.apellido
+      
