5ecebc2a1ff171f155baf3e300ece832a87ec766
Agustin Tocarczuk <agustintocarczuk@gmail.com>
2022-11-06 13:59:42 -0300

    Actualizacion nombre pesona

---
+++ modulo_persona.py
@@ -0,0 +1,11 @@
+class Persona:
+
+        def __init__(self, nombre, apellido):
+
+            self.nombre = nombre
+            self.apellido = apellido
+
+        def __str__(self):
+
+            return self.nombre + " " + self.apellido
+      
